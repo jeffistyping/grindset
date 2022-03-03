bfb8c11ee59dac314687b39a8aeb583df9c30586
Jeff Hui <jeffrey.kl.hui@gmail.com>
2022-03-02 20:58:34 -0500

    grind

---
+++ grindset.py
@@ -0,0 +1,15 @@
+class GrindSet(set):
+    
+    def fears():
+        return None
+
+    def insecurities():
+        return None
+
+    def doubts():
+        return None
+
+    def anxieties():
+        return None
+
+
